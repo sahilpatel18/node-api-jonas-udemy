7525b689e461cbd79aaa74a20df5ad3b3e351b1e
sahilpatel18 <sahil.41801@gmail.com>
2020-05-30 17:48:29 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,35 @@
+const fs = require("fs");
+const http = require("http");
+const url = require("url");
+
+const server = http.createServer((req, res) => {
+  console.log(req.url);
+
+  const pathName = req.url;
+
+  if (pathName === "/" || pathName === "/overview") {
+    res.end("This is the OVERVIEW");
+  } else if (pathName === "/product") {
+    res.end("this is the PRODUCT");
+  } else if (pathName === "/api") {
+    fs.readFile(
+      `${__dirname}/starter/dev-data/data.json`,
+      "utf-8",
+      (err, data) => {
+        const productData = JSON.parse(data);
+        res.writeHead(200, { "Content-type": "application/json" });
+        res.end(data);
+      }
+    );
+  } else {
+    res.writeHead(404, {
+      "Content-type": "text/html",
+      "my-own-header": "WHAT THE F!!!!!",
+    });
+    res.end("<h1>Page not found!!!</h1>");
+  }
+});
+
+server.listen(8000, "127.0.0.1", () => {
+  console.log("listening to port 8000 again!!!!");
+});
